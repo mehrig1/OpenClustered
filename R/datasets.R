7273a773f06a9f872407cc39a3e2d9aa9d4b4172
Nate OConnell <oconrunner@gmail.com>
2024-12-04 14:23:26 -0500

    edits

--- R/datasets.R
+++ R/datasets.R
@@ -1,19 +1,12 @@
 #' List of Longitduinal datasets
 #'
-#' Datasets are names "datX" where "X" is a number. Currently there are 19 individual longitudinal datasets. "data_meta" and "data_info" contain meta data and info 
+#' Datasets are names "datX" where "X" is a number. Currently there are 19 individual longitudinal datasets and a "meta_data" file summarizing them
 #'
-#' @format A tibble with 243 rows and 10 variables:
+#' @format A list with 19 datasets (prefixed with 'dat') and a summary of the datasets "meta_data. 
+#'         See the dataset "meta_data" within the list "datasets" (i.e. datasets$meta_data) for a description 
+#'         of each dataset, its source, and its meta data. 
 #' \describe{
-#'   \item{Year}{dbl Year price was recorded}
-#'   \item{Month}{dbl Month price was recorded. Ranges from 1-12 for January - December} 
-#'   \item{`0-15`}{dbl denoting monthly price for the 0-15/lb. size class}
-#'   \item{`15-20`}{dbl denoting monthly price for the 15-20/lb. size class}
-#'   \item{`20-25`}{dbl denoting monthly price for the 20-25/lb. size class}
-#'   \item{`25-30`}{dbl denoting monthly price for the 25-30/lb. size class}
-#'   \item{`30-40`}{dbl denoting monthly price for the 30-40/lb. size class}
-#'   \item{`40-50`}{dbl denoting monthly price for the 40-50/lb. size class}
-#'   \item{`50-67`}{dbl denoting monthly price for the 50-67/lb. size class}
-#'   \item{Pieces}{dbl denoting monthly price of pieces of shrimp totalling a pound}
-#' }
-#' @source \url{https://www.pnas.org/content/114/7/1512/tab-figures-data}
-"shrimp"
+#'   \item{target}{the outcome variable for each dataset}
+#'   \item{cluster_id}{the cluster variable for each dataset} 
+
+"datasets"
--- testing.R
+++ testing.R
@@ -1,4 +1,6 @@
 devtools::install_github("https://github.com/NateOConnellPhD/OpenClustered")
 
-
+library(OpenClustered)
+datasets$meta_data
+vis_datasets_smry()
 
