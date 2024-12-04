cf6d7fbd3c49571f01deca9914d1df034c4554f8
Nate OConnell <oconrunner@gmail.com>
2024-12-04 14:23:26 -0500

    edits

--- R/vis_datasets_smry.R
+++ R/vis_datasets_smry.R
@@ -8,10 +8,10 @@
 #' vis_datasets_smry(meta_data)
 #' @export
 #' 
-vis_datasets_smry <- function(df = datasets$data_meta){
+vis_datasets_smry <- function(df = datasets$meta_data){
   #Create plot for Coef of Variation
   p2 <- ggplot(df, (aes(x = n_features))) +
-    geom_histogram(binwidth = 1,
+    geom_histogram(
                    color = "white",
                    fill = "black",boundary = 0) +
     ggtitle("Number of Features") +
