a52e6bb3fb87d108d0c0d6a159a9fc5933904725
Nate OConnell <oconrunner@gmail.com>
2024-12-04 13:34:07 -0500

    edited datasets to a list

--- testing.R
+++ testing.R
@@ -1,5 +1,6 @@
-library(devtools)
-install()
 devtools::install_github("https://github.com/NateOConnellPhD/OpenClustered")
-
+library(OpenClustered)
+library(ggplot2)
+library(gridExtra)
+vis_datasets_smry(data_meta)
 
