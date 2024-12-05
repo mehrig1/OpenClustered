b69c401d348066120c7025883fce9940e1dcc653
Nate OConnell <oconrunner@gmail.com>
2024-12-05 09:53:43 -0500

    V1

--- R/plot_meta_data.R
+++ R/plot_meta_data.R
@@ -1,15 +1,24 @@
-#' Visualize summary characteristics of all available datasets 
+#' Provides plot of meta data for all datasets in the 'data_list'
 #'
-#' Creates a 4-panel figure of summary statistics of the dataset meta data, summarizes the total number of observations, number of features, number of clusters and imbalance. 
+#' Creates plots summarizing the meta data (n obervations, n features, n clusters, and imbalance) of the 19 datasets in the 'data_list'
+#' @param allplots Logical (T or F); If T, returns a 4-panel grid of each plot. If F, returns a list of each of the 4 plots as an individual element
 #' @param df the meta data file one returned from get_data("meta_data.csv");
 #' @return 4-panel figure of metadata summary 
 #' @examples 
-#' meta_data = get_data("meta_data")
-#' vis_datasets_smry(meta_data)
+#' plot_meta_data(allplots=T)
 #' @export
 #' 
-vis_datasets_smry <- function(df = OpenClustered::meta_data){
-  #Create plot for Coef of Variation
+plot_meta_data <- function(allplots=T, df = OpenClustered::meta_data){
+  #Plot for N obs
+  p1 <- ggplot(df, aes(x = n_obs)) +
+    geom_histogram(color = "white",
+                   fill = "black", boundary = 0) +
+    ggtitle("Number of Observations") +
+    ylab("Frequency") + xlab("N Observations") +
+    theme_bw() +
+    geom_hline(yintercept = 0)
+  
+  #N feature plot
   p2 <- ggplot(df, (aes(x = n_features))) +
     geom_histogram(
                    color = "white",
@@ -25,16 +34,7 @@ vis_datasets_smry <- function(df = OpenClustered::meta_data){
       axis.line.x.top = element_blank()
     )
   
-  #Plot for N:P
-  p1 <- ggplot(df, aes(x = n_obs)) +
-    geom_histogram(color = "white",
-                   fill = "black", boundary = 0) +
-    ggtitle("Number of Observations") +
-    ylab("Frequency") + xlab("N Observations") +
-    theme_bw() +
-    geom_hline(yintercept = 0)
-  
-  #create plot for number of features
+  #N cluster Plot
   p3 <- ggplot(df, aes(x = n_clusters)) +
     geom_histogram(
                    color = "white",
@@ -44,7 +44,7 @@ vis_datasets_smry <- function(df = OpenClustered::meta_data){
     theme_bw() +
     geom_hline(yintercept = 0)
   
-  #Create plot for number of instances
+  #Imbalance Plot
   p4 <- ggplot(df, (aes(x = imbalance))) +
     geom_histogram(
                    color = "white",
@@ -56,9 +56,19 @@ vis_datasets_smry <- function(df = OpenClustered::meta_data){
     geom_hline(yintercept = 0) +
     scale_y_continuous(breaks=c(1,2))
   
+  if(allplots==T){
   #Create combined plot
   grid.arrange(p1, p2, p3, p4,
                layout_matrix = matrix(c(1, 2, 3, 4),
                                       ncol = 2,
                                       byrow = T))
+  }
+  else {
+    return(
+      list(n_obs_plot = p1, 
+           n_feat_plot = p2,
+           n_cluster_plot = p3,
+           imb_plot = p4)
+    )
+  }
 }
