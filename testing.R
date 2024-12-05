564fd3294ee24bdef3a1daaba7c2b2ac98cd4f15
Nate OConnell <oconrunner@gmail.com>
2024-12-05 09:05:24 -0500

    fixing data list

--- testing.R
+++ testing.R
@@ -8,7 +8,9 @@ vis_datasets_smry()
 
 filter_data(n_features>10, n_obs>2500)
 
-
 library(roxygen2); # Read in the roxygen2 R package
 roxygenise();      # Builds the help files
+
+library(devtools);
+load_all("."); # Working directory should be in the package SCC_R_package
 
